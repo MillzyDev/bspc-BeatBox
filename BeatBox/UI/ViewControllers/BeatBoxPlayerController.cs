0672335e8aa54efdcf8bb72a3e66456cf2f05c81
Millzy <millzyyt08@gmail.com>
2022-04-11 18:30:29 +0100

    UI... attempt

---
+++ BeatBox/UI/ViewControllers/BeatBoxPlayerController.cs
@@ -0,0 +1,39 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace BeatBox.UI.ViewControllers
+{
+    class BeatBoxPlayerController : MonoBehaviour
+    {
+        [UIObject("bars")]
+        GameObject bars;
+
+        [UIObject("duration")]
+        GameObject duration;
+
+        [UIObject("barPrefab")]
+        GameObject barPrefab;
+
+        [UIComponent("durationSlider")]
+        SliderSetting durationSlider;
+
+        [UIComponent("time")]
+        TextMeshProUGUI time;
+
+        [UIComponent("length")]
+        TextMeshProUGUI length;
+
+        [UIAction("#post-parse")]
+        public void PostParse()
+        {
+
+        }
+    }
+}
