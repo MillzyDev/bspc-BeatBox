05b66a1b65d345f7f3d8d70524bf6da021b370ec
Millzy <millzyyt08@gmail.com>
2022-04-11 18:30:39 +0100

    plugin

---
+++ BeatBox/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BeatBox
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        private Harmony _harmony;
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("BeatBox initialized.");
+
+            _harmony = new Harmony("dev.Millzy.BeatBox");
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+
+            _harmony.PatchAll();
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            _harmony.UnpatchSelf();
+        }
+    }
+}
